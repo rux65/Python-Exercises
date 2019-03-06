0aa17141fb13192f0c3b57c12b1455100ce352c8
Ruxi <ruxi_65@yahoo.com>
2019-03-06 19:51:46 +0000

    Create les3.py

---
+++ les3.py
@@ -0,0 +1,25 @@
+#$cat nameoffile - brings out the content
+
+#$ ls - lists the files in the directory 
+
+#$ touch my_file.txt - creates a new file in the folder
+
+# $ cd - and type your folder starting with \C\ and bit by bit
+#      till you get into the folder you need to be 
+
+# $ mv my_file.txt my_newfile
+
+# $ mkdir - makes a new folder
+
+# $ mv file_name new_folder/
+
+# $ py -3 les3.py - it executed pyhton 
+
+
+
+favourite_fruits=["strawberry","banana","pear"]
+for next in favourite_fruits:
+	print(next)
+	
+
+	
