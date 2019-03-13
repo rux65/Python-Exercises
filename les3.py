50b31fe5aee9c74752a30160cf9029c6f69e56a4
Ruxi <ruxi_65@yahoo.com>
2019-03-13 22:24:13 +0000

    Update les3.py

--- les3.py
+++ les3.py
@@ -15,11 +15,32 @@
 
 # $ py -3 les3.py - it executed pyhton 
 
+# $ git clone + code address of your git account 
+# $ git clone + https://github.com/rux65/python_ex2.git
 
+#echo "comment" > newFile.txt - this will introduce lines in the file
+# $cat newFile.txt - shows you what is in the file 
 
+# $ git add . - adds the file- ! remeMber the space between add and "."
+#$ git status - lists status of git 
+
+#this is a commit of a new change:
+
+# git commit -m "message"
+# git push origin master  
+# git push origin master 
+
+#vim filename  - opens an editor in the console 
+
+#:wq - to close
+#:q! - if read only , then type this 
 favourite_fruits=["strawberry","banana","pear"]
 for next in favourite_fruits:
 	print(next)
+	
+
+	
+
 	
 
 	
